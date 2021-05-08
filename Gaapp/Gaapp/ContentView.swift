4068b0135a371babc70c5f1a7f7cbcff70fce899
Emilio Rivas <erivas@Emilios-MacBook-Pro.local>
2021-05-08 00:25:19 -0500

    Creacion de app

---
+++ Gaapp/Gaapp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Gaapp
+//
+//  Created by Emilio Rivas on 5/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hola GAAP")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
